bb23ef9956927ec7195d0030d5aa45fe82f8411b
bharath <bharath.cra@gmail.com>
2022-12-27 20:58:35 +0100

    Cucumber Framework

---
+++ src/test/java/utils/TestContextSetup.java
@@ -0,0 +1,28 @@
+package utils;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObjects.PageObjectManager;
+
+public class TestContextSetup {
+
+	public WebDriver driver;
+	public String landingPageProductName;
+	public PageObjectManager pageObjectManager;
+	public TestBase testBase;
+	public GenericUtils genericUtils;
+	public List<String> names;
+
+	
+	public TestContextSetup() throws IOException
+	{
+		testBase = new TestBase();
+		pageObjectManager = new PageObjectManager(testBase.WebDriverManager());
+		genericUtils = new GenericUtils(testBase.WebDriverManager());
+		names = new ArrayList<String>();
+	}
+}
